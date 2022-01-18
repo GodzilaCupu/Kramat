5c939e1463b2a88694378e93a2ab061cfa4ac7e3
Gdz.Cp-Pc <eryparasetyo.15@gmail.com>
2022-01-18 17:35:27 +0700

    Load Scene
    On Progress

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameManager instance;
+    [SerializeField] GameObject loadingCanvas;
+    
+
+    private void Awake()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void LoadLoadingScene()
     {
-        
+        if (SceneManager.GetActiveScene().buildIndex == (int)Enum.LoadingScene) loadingCanvas = GameObject.Find("LoadingCanvas");
+
+
     }
+
 }
