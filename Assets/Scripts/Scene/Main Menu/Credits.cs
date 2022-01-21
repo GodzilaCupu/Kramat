490b36a5ae115e3b2b4076d4eaa9a9eb8c9e7699
Gdz.Cp-Pc <prasetyo.djufri@gmail.com>
2022-01-21 22:26:19 +0700

    Credits Mockup

--- Assets/Scripts/Scene/Main Menu/Credits.cs
+++ Assets/Scripts/Scene/Main Menu/Credits.cs
@@ -4,16 +4,5 @@ using UnityEngine;
 
 public class Credits : MonoBehaviour
 {
-    private MainMenuManager menuManager;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
