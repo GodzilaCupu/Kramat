24a5c9ea6d02499d1bc0a4b4c9cbaf4a3a0dc9f6
GodzillaCupu <prasetyo.djufri@gmail.com>
2024-06-26 04:15:19 +0700

    Changelog - create clean project

--- Assets/Scripts/Loading/LoadingProgresSO.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Player Event Handler", menuName = "ScriptibleObject/Loading/Temporary Progres Handler")]
-public class LoadingProgresSO : ScriptableObject
-{
-    public float _loadingProgres;
-}
--- Assets/Scripts/Sound/WalkingScrptibleObject.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "SFX Player", menuName = "ScriptibleObject/Player/Step-SFX")]
-public class WalkingScrptibleObject : ScriptableObject
-{
-    public List<AudioClip> stepingSFX = new List<AudioClip>();
-}
